10f039197740fee498589f39ba09d550376710ce
ainul-hossain <ainulsakibs@gmail.com>
2023-12-17 01:04:12 +0600

    Flatlist single and multiple Select done!

--- App.js
+++ App.js
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  console.log('This line is from App.js...');
+
+  const [data, setData] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ])
+
+  const onSelect = (i) => {
+    const tempData = [];
+
+    data.map((item, index) => {
+      if (index === i) {
+        if (item === true) {
+          tempData.push(false);
+        } else {
+          tempData.push(true);
+        }
+      } else {
+        if (item === true) {
+          tempData.push(true);
+        } else {
+          tempData.push(false);
+        }
+      }
+    })
+
+    setData(tempData);
+
+    console.log(data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        style={{ marginTop: 100, marginBottom: 100 }}
+        renderItem={({ item, index }) => {
+
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                onSelect(index);
+              }}
+              style={{
+                width: '100%',
+                height: 70,
+                borderWidth: .2,
+                borderColor: 'gray',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: item === true ? 'green' : 'white'
+              }}
+            >
+              <Text style={{ fontSize: 30 }}>{`Item ${index + 1}`}</Text>
+            </TouchableOpacity>
+          )
+        }}
+      />
     </View>
   );
 }
@@ -14,7 +80,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
